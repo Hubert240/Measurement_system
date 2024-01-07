d828563a6ba8c5abf93f5d136270942127f11198
Hubert <szpiners@gmail.com>
2024-01-07 20:15:07 +0100

    zmiany

--- strona/projekt_st/czujniki/urls.py
+++ strona/projekt_st/czujniki/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('data/', views.data, name='data'),
+    path('temperature/', views.temperature, name='temperature'),
+    path('motion/', views.motion, name='motion'),
+    path('humidity/', views.humidity, name='humidity'),
+    path('', views.index, name='index'),
 ]
--- strona/projekt_st/czujniki/views.py
+++ strona/projekt_st/czujniki/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import Data,Motion
 
-def data(request):
-    data_entries = Data.objects.all()
-    motion = Motion.objects.all()
-    return render(request, 'data.html', {'data_entries': data_entries, 'motion': motion})
+
+def index(request):
+    return render(request, 'index.html')
+
+def temperature(request):
+    data = Data.objects.all()
+    return render(request, 'temperature.html', {'data': data})
+
+def humidity(request):
+    data = Data.objects.all()
+    return render(request, 'humidity.html', {'data': data})
+
+def motion(request):
+    data = Motion.objects.all()
+    return render(request, 'motion.html', {'data': data})
