257d174b0605a37de1897c6c2611acc2e7a20b76
Hubert <szpiners@gmail.com>
2024-01-14 14:38:05 +0100

    czas

--- strona/projekt_st/czujniki/static/js/motion.js
+++ strona/projekt_st/czujniki/static/js/motion.js
@@ -1,4 +1,4 @@
-        function formatujDate(ISODate) {
+ function formatujDate(ISODate) {
         const data = new Date(ISODate);
         const rok = data.getFullYear();
         const miesiac = (data.getMonth() + 1).toString().padStart(2, '0'); // Dodaj 1 do miesiąca, bo indeksowanie miesięcy zaczyna się od 0
@@ -9,27 +9,32 @@
         const sformatowanaData = `${rok}/${miesiac}/${dzien} ${godzina}:${minuta}:${sekunda}`;
         return sformatowanaData;
 }
-        function fetchData() {
-            $.ajax({
-                url: 'http://127.0.0.1:8000/api/motion/',
-                method: 'GET',
-                dataType: 'json',
-                success: function(data) {
-                    const motionTableBody = document.getElementById('motion-table-body');
-                    motionTableBody.innerHTML = '';
-
-                    data.forEach(x => {
-                        const row = document.createElement('tr');
 
 
-                        const motionCell = document.createElement('td');
-                        motionCell.textContent = x.motion;
-                        row.appendChild(motionCell);
 
 
-                        const dateCell = document.createElement('td');
-                        dateCell.textContent = x.date;
-                        row.appendChild(dateCell);
+
+ function fetchData() {
+    $.ajax({
+        url: 'http://127.0.0.1:8000/api/motion/',
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            const motionTableBody = document.getElementById('motion-table-body');
+            motionTableBody.innerHTML = '';
+
+            dataSort = data.sort((a, b) => new Date(b.date) - new Date(a.date));
+            data.forEach(x => {
+                const row = document.createElement('tr');
+
+                const motionCell = document.createElement('td');
+                motionCell.textContent = x.motion;
+                row.appendChild(motionCell);
+
+                const dateCell = document.createElement('td');
+                const sformatowanaData = formatujDate(x.date);
+                dateCell.textContent = sformatowanaData;
+                row.appendChild(dateCell);
 
                         motionTableBody.appendChild(row);
                     });
@@ -37,13 +42,15 @@
                 const timeSinceLastMotionElement = document.getElementById('time-since-last-motion');
 
                 if (data.length > 0) {
-                    const lastMotion = data[data.length - 1].motion;
-                    const lastMotionDate = new Date(data[data.length - 1].date);
-                    const sformatowanaData = formatujDate(new Date(data[data.length - 1].date));
+                    const lastMotion = dataSort[0];
+                    const lastMotionDate = new Date(data[0].date);
+                    lastMotionDate.setHours(lastMotionDate.getHours() - 1);
+                    const lastMotionDateUtcPlus2 = new Date(lastMotionDate.toLocaleString('en-US', { timeZone: 'Europe/Paris' }));
+                    const sformatowanaData = formatujDate(new Date(data[0].date));
                     const currentTime = new Date();
 
                     lastMotionElement.textContent = `Ostatni ruch: ${sformatowanaData}`;
-                    const timeDifference = currentTime - lastMotionDate;
+                    const timeDifference = currentTime - lastMotionDateUtcPlus2;
                     const secondsSinceLastMotion = Math.floor(timeDifference / 1000);
 
                     const hours = Math.floor(secondsSinceLastMotion / 3600);
