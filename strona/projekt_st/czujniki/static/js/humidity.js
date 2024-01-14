d7c7e7a1220413f967aedc9f55114e8763ebbfb0
Hubert <szpiners@gmail.com>
2024-01-14 12:57:03 +0100

    hour

--- strona/projekt_st/czujniki/static/js/humidity.js
+++ strona/projekt_st/czujniki/static/js/humidity.js
@@ -1,10 +1,27 @@
         var ctx = document.getElementById("masno");
+        var ctx2 = document.getElementById("masno2");
 
         var golddatas = {
             labels: [],
             datasets: [
                 {
-                    label: 'Humidity',
+                    label: 'Wilgotność',
+                    fillColor: 'rgb(255,255,122)',
+                    strokeColor: 'rgb(255,255,122)',
+                    borderColor: 'rgba(255,0,0,1)',
+                    pointBorderWidth: 2,
+                    pointRadius: 5,
+                    pointColor: 'rgba(127,150,199,0.5)',
+                    pointStrokeColor: 'rgba(127,150,199,0.5)',
+                    data: []
+                }
+            ]
+        };
+        var golddatas2 = {
+            labels: [],
+            datasets: [
+                {
+                    label: 'Średnia wilgotność - 7 dni',
                     fillColor: 'rgb(255,255,122)',
                     strokeColor: 'rgb(255,255,122)',
                     borderColor: 'rgba(255,0,0,1)',
@@ -18,8 +35,42 @@
         };
 
         var myChart = new Chart(ctx, {
+    type: 'line',
+    data: golddatas,
+    options: {
+        defaultFontSize: 10,
+        defaultFontColor: '#FFF',
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        scales: {
+            x: {
+                position: 'bottom',
+                title: {
+                    display: true,
+                    text: 'Godzina'
+                }
+            },
+            y: {
+                type: 'linear', // użyj linearnego skalowania dla osi y
+                ticks: {
+                    // Formatuj wartości osi y jako procenty
+                    callback: function (value, index) {
+                        return value + '%';
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Procent'
+                }
+            }
+        }
+    }
+});
+        var myChart2 = new Chart(ctx2, {
             type: 'line',
-            data: golddatas,
+            data: golddatas2,
             options: {
                 defaultFontSize: 10,
                 defaultFontColor: '#FFF',
@@ -27,7 +78,28 @@
                 legend: {
                     display: false,
                 },
-
+            scales: {
+            x: {
+                position: 'bottom',
+                title: {
+                    display: true,
+                    text: 'Data'
+                }
+            },
+            y: {
+                type: 'linear', // użyj linearnego skalowania dla osi y
+                ticks: {
+                    // Formatuj wartości osi y jako procenty
+                    callback: function (value, index) {
+                        return value + '%';
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Procent'
+                }
+            }
+        }
 
             }
         });
@@ -36,6 +108,11 @@
     myChart.data.datasets[0].data = newData;
     myChart.update();
 }
+ function updateChart2(newData, counter) {
+    myChart2.data.labels = counter;
+    myChart2.data.datasets[0].data = newData;
+    myChart2.update();
+}
 
 
     function formatujDate(ISODate) {
@@ -43,13 +120,88 @@
         const rok = data.getFullYear();
         const miesiac = (data.getMonth() + 1).toString().padStart(2, '0'); // Dodaj 1 do miesiąca, bo indeksowanie miesięcy zaczyna się od 0
         const dzien = data.getDate().toString().padStart(2, '0');
-        const godzina = data.getHours().toString().padStart(2, '0');
+        const godzina = (data.getHours() - 1).toString().padStart(2, '0');
         const minuta = data.getMinutes().toString().padStart(2, '0');
         const sekunda = data.getSeconds().toString().padStart(2, '0');
         const sformatowanaData = `${rok}/${miesiac}/${dzien} ${godzina}:${minuta}:${sekunda}`;
         return sformatowanaData;
 }
+function roundToTwoDecimalPlaces(number) {
+    return Math.round((number + Number.EPSILON) * 100) / 100;
+}
+function processDataForLast7Days(data) {
+    const currentDate = new Date();
 
+    // Ustaw datę na koniec dzisiejszego dnia
+    currentDate.setHours(23, 59, 59, 999);
+
+    const sevenDaysAgo = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000); // Oblicz datę sprzed 6 dni, aby mieć łącznie 7 dni
+
+    const filteredData = data.filter(item => {
+        const itemDate = new Date(item.date);
+        return itemDate >= sevenDaysAgo && itemDate <= currentDate; // Ograniczenie do ostatnich 7 dni
+    });
+
+    const datesSet = new Set(); // Użyj Set do przechowywania unikalnych dat
+    const averagesMap = new Map(); // Użyj Map do przechowywania par daty i średniej
+
+    filteredData.forEach(item => {
+        const itemDate = new Date(item.date).toISOString().split('T')[0]; // Formatowanie do "YYYY-MM-DD"
+        datesSet.add(itemDate);
+    });
+
+    // Sortuj daty od najstarszej do najmłodszej
+    const sortedDatesArray = Array.from(datesSet).sort();
+
+    sortedDatesArray.forEach(date => {
+        const valuesForDate = filteredData.filter(item => {
+            const itemDate = new Date(item.date).toISOString().split('T')[0];
+            return itemDate === date;
+        });
+
+        const averageValue = calculateAverageTemperature(valuesForDate); // Zastąp funkcją z obliczaniem średniej wartości dla danego pola
+        const roundedAverage = roundToTwoDecimalPlaces(averageValue);
+        averagesMap.set(date, roundedAverage);
+    });
+
+    // Skróć wyniki do ostatnich 7 dni
+    const last7DatesArray = sortedDatesArray.slice(-7);
+    const last7AveragesArray = last7DatesArray.map(date => averagesMap.get(date));
+
+    return { dates: last7DatesArray, averages: last7AveragesArray };
+}
+function getLast15TemperaturesAndHours(data) {
+    const last20Data = data.slice(-15); // Pobierz ostatnie 20 danych
+
+    const temperaturesArray = [];
+    const hoursArray = [];
+
+    last20Data.forEach(item => {
+        const itemDate = new Date(item.date);
+        const temperature = item.humidity;
+
+        // Dodaj godzinę do tablicy godzin w formacie "HH:mm"
+        const hour = ('0' + itemDate.getHours()).slice(-2);
+        const minutes = ('0' + itemDate.getMinutes()).slice(-2);
+        const timeString = `${hour}:${minutes}`;
+
+        temperaturesArray.push(temperature);
+        hoursArray.push(timeString);
+    });
+
+    // Odwróć tablice, aby mieć od najstarszej do najmłodszej
+
+
+
+    return { temperatures: temperaturesArray, hours: hoursArray };
+}
+
+
+// Funkcja do obliczania średniej temperatury
+function calculateAverageTemperature(data) {
+    const totalTemperature = data.reduce((sum, item) => sum + item.humidity, 0);
+    return totalTemperature / data.length;
+}
 
     function fetchData() {
         $.ajax({
@@ -106,9 +258,12 @@
 
 
                 data.reverse();
-                const humidityValues = Object.values(data).map(item => item.humidity);
-                console.log(humidityValues);
-                updateChart(humidityValues, array);
+                const result2 = getLast15TemperaturesAndHours(data);
+                updateChart(result2.temperatures, result2.hours);
+                const result = processDataForLast7Days(data);
+                updateChart2(result.averages, result.dates);
+                console.log(result.dates);
+                console.log(result.averages);
             },
             error: function(error) {
                 console.error('Błąd pobierania danych z API:', error);
--- strona/projekt_st/czujniki/static/js/temperature.js
+++ strona/projekt_st/czujniki/static/js/temperature.js
@@ -1,10 +1,27 @@
         var ctx = document.getElementById("masno");
+        var ctx2 = document.getElementById("masno2");
 
         var golddatas = {
             labels: [],
             datasets: [
                 {
-                    label: 'Temperature',
+                    label: 'Temperatura',
+                    fillColor: 'rgb(255,255,122)',
+                    strokeColor: 'rgb(255,255,122)',
+                    borderColor: 'rgba(255,0,0,1)',
+                    pointBorderWidth: 2,
+                    pointRadius: 5,
+                    pointColor: 'rgba(127,150,199,0.5)',
+                    pointStrokeColor: 'rgba(127,150,199,0.5)',
+                    data: []
+                }
+            ]
+        };
+         var golddatas2 = {
+            labels: [],
+            datasets: [
+                {
+                    label: 'Średnia temperatura - 7 dni',
                     fillColor: 'rgb(255,255,122)',
                     strokeColor: 'rgb(255,255,122)',
                     borderColor: 'rgba(255,0,0,1)',
@@ -27,7 +44,63 @@
                 legend: {
                     display: false,
                 },
+            scales: {
+            x: {
+                position: 'bottom',
+                title: {
+                    display: true,
+                    text: 'Godzina'
+                }
+            },
+            y: {
+                type: 'linear', // użyj linearnego skalowania dla osi y
+                ticks: {
+                    // Formatuj wartości osi y jako procenty
+                    callback: function (value, index) {
+                        return value + ' °C ';
+                    }
+                },
+                title: {
+                    display: true,
+                    text: '°C'
+                }
+            }
+        }
 
+            }
+        });
+        var myChart2 = new Chart(ctx2, {
+            type: 'line',
+            data: golddatas2,
+            options: {
+                defaultFontSize: 10,
+                defaultFontColor: '#FFF',
+                maintainAspectRatio: false,
+                legend: {
+                    display: false,
+                },
+            scales: {
+            x: {
+                position: 'bottom',
+                title: {
+                    display: true,
+                    text: 'Data'
+                }
+            },
+            y: {
+                type: 'linear', // użyj linearnego skalowania dla osi y
+                ticks: {
+                    // Formatuj wartości osi y jako procenty
+                    callback: function (value, index) {
+                        return value + '°C';
+                    }
+                },
+                title: {
+                    display: true,
+                    text: '°C'
+                }
+            }
+        }
 
             }
         });
@@ -36,14 +109,93 @@
     myChart.data.datasets[0].data = newData;
     myChart.update();
 }
+ function updateChart2(newData, counter) {
+    myChart2.data.labels = counter;
+    myChart2.data.datasets[0].data = newData;
+    myChart2.update();
+}
+function roundToTwoDecimalPlaces(number) {
+    return Math.round((number + Number.EPSILON) * 100) / 100;
+}
+function processDataForLast7Days(data) {
+    const currentDate = new Date();
 
+    // Ustaw datę na koniec dzisiejszego dnia
+    currentDate.setHours(23, 59, 59, 999);
+
+    const sevenDaysAgo = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000); // Oblicz datę sprzed 6 dni, aby mieć łącznie 7 dni
+
+    const filteredData = data.filter(item => {
+        const itemDate = new Date(item.date);
+        return itemDate >= sevenDaysAgo && itemDate <= currentDate; // Ograniczenie do ostatnich 7 dni
+    });
+
+    const datesSet = new Set(); // Użyj Set do przechowywania unikalnych dat
+    const averagesMap = new Map(); // Użyj Map do przechowywania par daty i średniej
+
+    filteredData.forEach(item => {
+        const itemDate = new Date(item.date).toISOString().split('T')[0]; // Formatowanie do "YYYY-MM-DD"
+        datesSet.add(itemDate);
+    });
+
+    // Sortuj daty od najstarszej do najmłodszej
+    const sortedDatesArray = Array.from(datesSet).sort();
+
+    sortedDatesArray.forEach(date => {
+        const valuesForDate = filteredData.filter(item => {
+            const itemDate = new Date(item.date).toISOString().split('T')[0];
+            return itemDate === date;
+        });
+
+        const averageValue = calculateAverageTemperature(valuesForDate); // Zastąp funkcją z obliczaniem średniej wartości dla danego pola
+        const roundedAverage = roundToTwoDecimalPlaces(averageValue);
+        averagesMap.set(date, roundedAverage);
+    });
+
+    // Skróć wyniki do ostatnich 7 dni
+    const last7DatesArray = sortedDatesArray.slice(-7);
+    const last7AveragesArray = last7DatesArray.map(date => averagesMap.get(date));
+
+    return { dates: last7DatesArray, averages: last7AveragesArray };
+}
+function getLast15TemperaturesAndHours(data) {
+    const last20Data = data.slice(-15); // Pobierz ostatnie 20 danych
+
+    const temperaturesArray = [];
+    const hoursArray = [];
+
+    last20Data.forEach(item => {
+        const itemDate = new Date(item.date);
+        const temperature = item.temperature;
+
+        // Dodaj godzinę do tablicy godzin w formacie "HH:mm"
+        const hour = ('0' + itemDate.getHours()).slice(-2);
+        const minutes = ('0' + itemDate.getMinutes()).slice(-2);
+        const timeString = `${hour}:${minutes}`;
+
+        temperaturesArray.push(temperature);
+        hoursArray.push(timeString);
+    });
+
+    // Odwróć tablice, aby mieć od najstarszej do najmłodszej
+
+
+    return { temperatures: temperaturesArray, hours: hoursArray };
+}
+
+
+// Funkcja do obliczania średniej temperatury
+function calculateAverageTemperature(data) {
+    const totalTemperature = data.reduce((sum, item) => sum + item.temperature, 0);
+    return totalTemperature / data.length;
+}
 
     function formatujDate(ISODate) {
         const data = new Date(ISODate);
         const rok = data.getFullYear();
         const miesiac = (data.getMonth() + 1).toString().padStart(2, '0'); // Dodaj 1 do miesiąca, bo indeksowanie miesięcy zaczyna się od 0
         const dzien = data.getDate().toString().padStart(2, '0');
-        const godzina = data.getHours().toString().padStart(2, '0');
+        const godzina = (data.getHours() - 1).toString().padStart(2, '0');
         const minuta = data.getMinutes().toString().padStart(2, '0');
         const sekunda = data.getSeconds().toString().padStart(2, '0');
         const sformatowanaData = `${rok}/${miesiac}/${dzien} ${godzina}:${minuta}:${sekunda}`;
@@ -61,6 +213,8 @@
                 let counter = 0;
                 let array = []
                 humidityTableBody.innerHTML = '';
+
+
                 let totalTemperature = 0;
                 let maxTemperature = 0;
                 let temperatures = [];
@@ -105,9 +259,12 @@
                 medianTemperatureElement.textContent = `Mediana temperatury: ${medianTemperature.toFixed(2)}`;
 
                 data.reverse();
-                const humidityValues = Object.values(data).map(item => item.temperature);
-                console.log(humidityValues);
-                updateChart(humidityValues, array);
+                const result2 = getLast15TemperaturesAndHours(data);
+                updateChart(result2.temperatures, result2.hours);
+                const result = processDataForLast7Days(data);
+                updateChart2(result.averages, result.dates);
+                console.log(result.dates);
+                console.log(result.averages);
             },
             error: function(error) {
                 console.error('Błąd pobierania danych z API:', error);
